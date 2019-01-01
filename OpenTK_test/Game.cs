29aadfc33fe2e75d90516781fe59d57e59a63668
Hugo Duarte <urukyacc@gmail.com>
2019-01-01 13:31:04 +0000

    Add project files.

---
+++ OpenTK_test/Game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace OpenTK_test
+{
+    class Game
+    {
+        private GameWindow window;
+
+        public Game(GameWindow window)
+        {
+            this.window = window;
+            this.Start();
+        }
+
+        public void Start()
+        {
+            window.Load += Loaded;
+            window.Resize += Resize;
+            window.RenderFrame += RenderF;
+            window.Run(1.0, 60.0);
+        }
+
+        void Resize(object o, EventArgs e)
+        {
+            GL.Viewport(0,0, window.Width,window.Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0.0,50.0,0.0,50.0, -1.0, 1.0);
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+
+        void RenderF(object o, EventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Begin(BeginMode.Triangles);
+            GL.Vertex2(1.0, 1.0);
+            GL.Vertex2(49.0, 1.0);
+            GL.Vertex2(25.0, 49.0);
+            GL.End();
+            window.SwapBuffers();
+        }
+
+        void Loaded(object o, EventArgs e)
+        {
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        }
+    }
+}
---
+++ OpenTK_test/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using OpenTK;
+
+namespace OpenTK_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           GameWindow window = new GameWindow(800, 600);
+           Game gm = new Game(window);
+
+        }
+    }
+}
